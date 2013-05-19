1df4172afd525bf30b730392219befcc849f7cd1
Alex Gurrola <alex.gurrola@versidyne.com>
2013-05-19 02:12:41 -0700

    Added Designer Gateway
    This file will serve as a place marker and outline.

---
+++ gateways/des.php
@@ -0,0 +1,7 @@
+<?php
+	
+	// This is meant to be a design gateway, similar to the admin panel
+	// This file will use various modules and allow a designer to build
+	// their website, visually, within their browser.
+	
+?>
