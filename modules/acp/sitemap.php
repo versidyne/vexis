667311db466dfef0b7a1e1e3271764b3a4a03504
Alex Gurrola <alex.gurrola@versidyne.com>
2013-05-19 02:09:53 -0700

    Added Sitemap Module
    This file will serve as a place marker and outline.

---
+++ modules/acp/sitemap.php
@@ -0,0 +1,9 @@
+<?php
+	
+	// This will house the sitemap.xml/robots.txt generator
+	
+	// At some point in the future, this will have an update feature
+	// that will automatically update the sitemap when posts or pages
+	// are created, deleted, or edited.
+	
+?>
