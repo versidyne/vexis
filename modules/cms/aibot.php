ee4a59867d4610047f7e9abe0a9a0c9fbe60dbdb
alexgurrola <alex.gurrola@versidyne.com>
2013-05-29 04:24:25 -0700

    Added expansion info

--- modules/cms/aibot.php
+++ modules/cms/aibot.php
@@ -3,6 +3,11 @@
 	if ($validator['code'] != md5($validator['key'])) { echo "Please do not access this file directly."; exit; }
 
 	if ($_POST['submit'] == "true") {
+		// this is a simple example that should be used as a basis for gathering directional data in english
+		// for instance, I, me, is talking about the person while you, your, is talking about the bot.
+		// if we can simplify these tasks, we can make simple strings that the bot will translate to an answer
+		// with help from the mysql database table, of course
+		// if this goes well, it can be used to guide a user through the website or ask questions in a knowledgebase
 		if (preg_match("/(hello|hi)/i", "{$_POST['query']}")) {
 			$form_message = "Hello.  What might you be curious about?";
 		} elseif (preg_match("/How.are.you/i", "{$_POST['query']}")) {
