40c57ce553e716c8932207bc0142d6d20293b735
Alex Gurrola <alex.gurrola@versidyne.com>
2013-05-31 20:14:10 -0700

    Added Size Unit Conversion

--- libraries/data.php
+++ libraries/data.php
@@ -113,6 +113,36 @@
 			$cname = $settings["cookie_prefix"].$name;
 			setcookie ($cname, $data, time()+$expiration, $settings["cookie_directory"], $settings["cookie_website"], 0);
 		}
+		
+		public function sizeunits ($bytes) {
+			if ($bytes >= 1073741824)
+			{
+				$bytes = number_format($bytes / 1073741824, 2) . ' GB';
+			}
+			elseif ($bytes >= 1048576)
+			{
+				$bytes = number_format($bytes / 1048576, 2) . ' MB';
+			}
+			elseif ($bytes >= 1024)
+			{
+				$bytes = number_format($bytes / 1024, 2) . ' KB';
+			}
+			elseif ($bytes > 1)
+			{
+				$bytes = $bytes . ' bytes';
+			}
+			elseif ($bytes == 1)
+			{
+				$bytes = $bytes . ' byte';
+			}
+			else
+			{
+				$bytes = '0 bytes';
+			}
+
+			return $bytes;
+		}
+
 	}
 
 ?>
