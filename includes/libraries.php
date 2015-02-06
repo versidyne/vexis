643b50087b62dfca777b6a344236da0af2efac74
Alex Gurrola <alex.gurrola@versidyne.com>
2015-02-06 13:29:56 -0800

    Cleaned up Includes

--- includes/libraries.php
+++ includes/libraries.php
@@ -1,6 +1,5 @@
 <?php
 	
-	// Load Packages
 	function __autoload($class) {
 		$library = "libraries/{$class}.php";
 		if (file_exists($library)) {
@@ -10,15 +9,9 @@
 		}
 	}
 	
-	// Set Validation Variable
 	$config['security']['validator'] = md5($config['security']['key']);
 	
 	// Legacy Validation Variable
 	$validator = array("key" => $config['security']['key'], "code" => md5($config['security']['key']));
-	
-	// Legacy Library Loader
-	/*$libdir = opendir("libraries");
-	while ($fn = readdir($libdir)) { if (substr($fn, -4, 4) == ".php") { require "libraries" . "/" . $fn; } }
-	closedir($libdir);*/
 	
 ?>
--- includes/vfs.php
+++ includes/vfs.php
@@ -1,18 +1,12 @@
 <?php
 	
-	// Declare array
 	$vfs = array();
 	
-	// Evaluate Virtual File System
 	if (isset($_SERVER['REQUEST_URI'])) {
 		
-		// Gather path data
 		$vfs['path'] = $_SERVER['REQUEST_URI'];
-		
-		// Split by token
 		$vpath = explode("/", $vfs['path']);
 		
-		// Build names
 		$vfs['parent'] = $vpath[1];
 		$vfs['name'] = $vpath[2];
 		
